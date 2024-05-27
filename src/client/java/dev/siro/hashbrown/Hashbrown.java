e83cad2efbd5c3e70ceaf61d58baf530e5091117
Preta-Crowz <preta@siro.dev>
2024-05-28 03:01:00 +0900

    add missing logger import

--- src/client/java/dev/siro/hashbrown/Hashbrown.java
+++ src/client/java/dev/siro/hashbrown/Hashbrown.java
@@ -1,6 +1,8 @@
 package dev.siro.hashbrown;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Hashbrown implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("hashbrown");
